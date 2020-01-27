054dfe2f72d5625e2ee34c4b406894028a5c6629
Jean Gautier <jgautier@oui.sncf>
2020-01-27 11:45:38 +0100

    initilise the dojo with first test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+import {todoList} from "./store/todo-list";
+import InputTodo from "./components/input/input";
+
+const store = createStore(todoList);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Provider store={store}>
+          <InputTodo />
+        </Provider>
       </header>
     </div>
   );
---
+++ src/store/todo-list.js
@@ -0,0 +1,8 @@
+import {handleActions} from "redux-actions";
+
+export const defaultState = {};
+
+export const todoList = handleActions(
+    {},
+    defaultState
+);
